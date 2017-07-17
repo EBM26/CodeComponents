14fc385b7c96d2347543221974b1dc8baa43dfc4
Eyal Benmoshe <ebm2626@yahoo.com>
2017-07-17 11:52:44 -0700

    making sure nothing is not committed

---
+++ GruntTest/main.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+	$(".one").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(0deg)",
+						"-moz-transform": "rotate(0deg)",
+					   " -ms-transform": "rotate(0deg)",
+						"-o-transform": "rotate(0deg)",
+						"transform": "rotate(0deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".two").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-30deg)",
+						"-moz-transform": "rotate(-30deg)",
+					   " -ms-transform": "rotate(-30deg)",
+						"-o-transform": "rotate(-30deg)",
+						"transform": "rotate(-30deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".three").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-60deg)",
+						"-moz-transform": "rotate(-60deg)",
+					   " -ms-transform": "rotate(-60deg)",
+						"-o-transform": "rotate(-60deg)",
+						"transform": "rotate(-60deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".four").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-90deg)",
+						"-moz-transform": "rotate(-90deg)",
+					   " -ms-transform": "rotate(-90deg)",
+						"-o-transform": "rotate(-90deg)",
+						"transform": "rotate(-90deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".five").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-120deg)",
+						"-moz-transform": "rotate(-120deg)",
+					   " -ms-transform": "rotate(-120deg)",
+						"-o-transform": "rotate(-120deg)",
+						"transform": "rotate(-120deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".six").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-150deg)",
+						"-moz-transform": "rotate(-150deg)",
+					   " -ms-transform": "rotate(-150deg)",
+						"-o-transform": "rotate(-150deg)",
+						"transform": "rotate(-150deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".seven").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-180deg)",
+						"-moz-transform": "rotate(-180deg)",
+					   " -ms-transform": "rotate(-180deg)",
+						"-o-transform": "rotate(-180deg)",
+						"transform": "rotate(-180deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".eight").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-210deg)",
+						"-moz-transform": "rotate(-210deg)",
+					   " -ms-transform": "rotate(-210deg)",
+						"-o-transform": "rotate(-210deg)",
+						"transform": "rotate(-210deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".nine").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-240deg)",
+						"-moz-transform": "rotate(-240deg)",
+					   " -ms-transform": "rotate(-240deg)",
+						"-o-transform": "rotate(-240deg)",
+						"transform": "rotate(-240deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".ten").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-270deg)",
+						"-moz-transform": "rotate(-270deg)",
+					   " -ms-transform": "rotate(-270deg)",
+						"-o-transform": "rotate(-270deg)",
+						"transform": "rotate(-270deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+	$(".eleven").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-300deg)",
+						"-moz-transform": "rotate(-300deg)",
+					   " -ms-transform": "rotate(-300deg)",
+						"-o-transform": "rotate(-300deg)",
+						"transform": "rotate(-300deg)"});
+						
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+
+	});
+
+	$(".twelve").click(function() {
+		$(".turn").css({" -webkit-transform": "rotate(-330deg)",
+						"-moz-transform": "rotate(-330deg)",
+					   " -ms-transform": "rotate(-330deg)",
+						"-o-transform": "rotate(-330deg)",
+						"transform": "rotate(-330deg)"});
+
+		$("body").fadeOut(1500, function() {
+			window.location="individual-sign.html";
+		});
+	});
+
+});
